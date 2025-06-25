bb3708e2d5b880da084b28a6879b3436e066a6be
WiktoriaRebeka <wiktoriarebeka00@gmail.com>
2025-06-25 16:54:29 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -14,8 +14,7 @@ LAST_FETCH_STATE_DOC_ID = "last_fetch_state"
 LAST_PROCESSED_TS_FIELD = "last_processed_firestore_timestamp"
 
 # NOWA, GŁÓWNA KOLEKCJA DO ZARZĄDZANIA STANEM TRANSAKCJI
-SETUP_COLLECTION = "active_setups"
-TRADE_COLLECTION = "active_trades"
+STATE_COLLECTION = "active_setups"
 
 # --- Bybit API ---
 BYBIT_API_URL_V5_TICKERS = "https://api.bybit.com/v5/market/tickers"
