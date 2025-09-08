fb7df334d7bfad3bbf09a798c9b1112d559298a6
WiktoriaRebeka <wiktoriarebeka00@gmail.com>
2025-09-08 20:50:18 +0200

    update constants.py

--- shared_lib/constants.py
+++ shared_lib/constants.py
@@ -8,7 +8,7 @@ FIRESTORE_COLLECTION_ALERTS = "alerts"
 BOT_CONFIG_COLLECTION = "bot_config"
 LATEST_KLINES_COLLECTION = "latest_klines"
 ANALYTICAL_CASES_COLLECTION = "analytical_cases"
-ACTIVE_ORDERS_COLLECTION = "active_orders"  # <-- Dodana stała dla aktywnych zleceń
+ACTIVE_ORDERS_COLLECTION = "active_orders"  # <-- TA LINIA MUSI ISTNIEĆ
 
 # Nazwy dokumentów i pól
 SYMBOLS_CONFIG_DOC_ID = "symbols_config"
@@ -17,10 +17,8 @@ LAST_PROCESSED_TS_FIELD = "last_processed_timestamp"
 # === Konfiguracja BigQuery ===
 BIGQUERY_PROJECT_ID = os.getenv("GCP_PROJECT", "trading-bot-463318")
 BIGQUERY_DATASET_ID = "trading_analytics"
-# Tabela dla analizy historycznej (backtestu)
 BIGQUERY_ANALYTICAL_TABLE_ID = "new_trades_history"
-# Tabela dla wyników realnego handlu
-BIGQUERY_REAL_TRADES_TABLE_ID = "real_trades_history" # <-- Dodana stała dla realnych transakcji
+BIGQUERY_REAL_TRADES_TABLE_ID = "real_trades_history"
 
 # === Konfiguracja API ===
 BYBIT_API_URL_V5_KLINE = "https://api.bybit.com/v5/market/kline"
